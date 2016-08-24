f959eddf64a81344aae34c2324d6c48a49fcc93f
Guilherme <guilhermefernandes1@gmail.com>
2016-08-24 00:37:59 -0300

    Adicao de seuranca

--- src/main/resources/static/javascripts/vinho.upload-foto.js
+++ src/main/resources/static/javascripts/vinho.upload-foto.js
@@ -1,19 +1,44 @@
-$(function() {
+var Wine = Wine || {};
+
+Wine.UploadFoto = (function() { 
 	
-	var uploadDrop = $('#upload-drop');
-	var containerFoto = $('.js-container-foto');
+	function UploadFoto() {
+		this.uploadDrop = $('#upload-drop');
+		this.containerFoto = $('.js-container-foto');
+	}
 	
-	var settings = {
+	UploadFoto.prototype.iniciar = function() {
+		var settings = {
 			type: 'json',
 			filelimit: 1,
 			allow: '*.(jpg|jpeg|png)',
-			action: '/fotos/' + uploadDrop.data('codigo'),
-			complete: function(foto) {
-				uploadDrop.addClass('hidden');
-				containerFoto.prepend('<img src="' + foto.url + '" class="img-responsive" style="margin: auto"/>');
-			}
+			action: '/fotos/' + this.uploadDrop.data('codigo'),
+			complete: onUploadCompleto.bind(this), 
+			beforeSend: adicionarCsrfToken
 		};
 		
 		UIkit.uploadSelect($('#upload-select'), settings);
-		UIkit.uploadDrop(uploadDrop, settings);
+		UIkit.uploadDrop(this.uploadDrop, settings);
+	}
+	
+	function adicionarCsrfToken(xhr) {
+		var header = $('input[name=_csrf_header]').val();
+		var token = $('input[name=_csrf]').val();
+		xhr.setRequestHeader(header, token);
+	}
+	
+	function onUploadCompleto(foto) {
+		this.uploadDrop.addClass('hidden');
+		this.containerFoto.prepend('<img src="' + foto.url + '" class="img-responsive" style="margin: auto"/>');
+	}
+	
+	return UploadFoto;
+	
+})();
+
+$(function() {
+	
+	var uploadFoto = new Wine.UploadFoto();
+	uploadFoto.iniciar();
+	
 });
